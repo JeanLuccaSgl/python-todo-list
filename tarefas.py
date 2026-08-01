79cc1a28e4ad2a25d1f36e5a65377949f66522e5
JeanLuccaSgl <jeanluccasgl@gmail.com>
2026-08-01 17:07:51 -0300

    Improve input validation

--- tarefas.py
+++ tarefas.py
@@ -8,25 +8,31 @@ def adicionarTarefa(listaTarefas):
 
     geradorId += 1
 
-    nomeTarefa = input("Digite o nome da sua tarefa: ")
+    while True:                                                       #Verificação se o campo nomeTarefa está preenchido
+        nomeTarefa = input("Digite o nome da sua tarefa: ").strip
 
-    statusTarefa = False
+        if nomeTarefa:
+            break
 
-    infoDict = {
+        print("O nome da tarefa não pode estar vazio.")
+
+    statusTarefa = False             #Define o status como falso/não concluído automaticamente
+
+    infoDict = {                     #Criação do dicionário
         "id": geradorId,
         "nome": nomeTarefa,
         "status": statusTarefa,
     }
 
-    listaTarefas.append(infoDict)
+    listaTarefas.append(infoDict)             #Inclui o novo dicionário/tarefa na lista
     print("Tarefa adicionada com sucesso!")
 
 
 def listarTarefa(listaTarefas):
-    if not listaTarefas: #verificação se a lista tá vazia, retorna True se tiver, logo, não precisa ter o == True
+    if not listaTarefas: #Verificação se a lista tá vazia, retorna True se tiver, logo, não precisa ter o == True
         print("Nenhuma tarefa encontrada")
 
-    for tarefa in listaTarefas:
+    for tarefa in listaTarefas: #Percorre a lista de tarefas e se alguma estiver com status True/Concluído ele marca com [X], senão com [ ]
         if tarefa["status"]:
            print(f'[X] {tarefa["id"]} - {tarefa["nome"]}')
         else:
@@ -34,35 +40,50 @@ def listarTarefa(listaTarefas):
 
 
 def concluirTarefa(listaTarefas):
-    encontrou = False
+    encontrou = False        #Variável para verificação se o id realmente existe, inicialmente não encontrou
 
-    idUsuario = int(input("Digite o ID da tarefa que você quer mudar o status: "))
+    while True:              #Loop e verificação de valor do usuário, se preencher sem um número, mostra mensagem e pede pra digitar novamente
+        try:
+            idUsuario = int(input("Digite o ID da tarefa que você quer mudar o status: "))
+            break
 
-    for tarefa in listaTarefas:
+        except ValueError:
+            print("O campo deve ser preenchido corretamente.")
+
+    for tarefa in listaTarefas:             #Loop pra verificar o id da tarefa digitada pelo usuário, se encontrar altera o status de "encontrou" pra True
         if tarefa["id"] == idUsuario:
             encontrou = True
-            if tarefa["status"]:
+
+            if tarefa["status"]:                          #Verificação se a tarefa já foi concluída ou não, se não foi altera o status e mostra mensagem
                 print("A tarefa já foi concluída!")
                 break
+
             else:
                 tarefa["status"] = True
                 print("Tarefa marcada como concluída com sucesso!")
 
             break
 
-    if not encontrou:
+    if not encontrou:                                           #Se não existe nenhuma tarefa com aquele id o sistema informa
         print("Não existe nenhuma tarefa com esse ID.")
 
 
-def removerTarefa(listaTarefas):
+def removerTarefa(listaTarefas):                                #Lógica semelhante a função de concuir tarea, mas removendo
     encontrou = False
 
-    idUsuario = int(input("Digite o ID da tarefa que você quer excluir a tarefa: "))
+    while True:
+        try:
+            idUsuario = int(input("Digite o ID da tarefa que você quer excluir: "))
+            break
+        except ValueError:
+            print("O campo deve ser preenchido corretamente.")
 
     for tarefa in listaTarefas:
         if tarefa["id"] == idUsuario:
             encontrou = True
+
             listaTarefas.remove(tarefa)
+
             print("Tarefa removida com sucesso!")
             break
 
